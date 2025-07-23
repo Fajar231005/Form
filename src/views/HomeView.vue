684e98cdf7d8efdc43c141416f84dfde1dc817a7
Fajar Gilang <realmerama243@gmail.com>
2025-07-23 14:41:58 +0700

    api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 
+interface Event {
+  id: number
+  name: string
+  place: string
+  time: string
+}
+
+const events = ref<Event[]>([])
+
+const fetchEvents = async () => {
+  const response = await fetch('/api/events')
+  const data = await response.json()
+  events.value = data
+}
+
+onMounted(() => {
+  fetchEvents()
+})
 </script>
+
+
 
 <template>
   <main>
     Home
+
+    <div>Pendaftaran pondok pesantren Alberr</div>
+
+    <div>
+      <ul>
+        <li v-for="event in events" :key="event.id">
+          <div>{{ event.name }}</div>
+          <div>{{ event.place }}</div>
+          <div>{{ event.time }}</div>
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
