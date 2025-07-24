223b2be014d2752d6efcd3b92d5b85a857220c60
Fajar Gilang <realmerama243@gmail.com>
2025-07-24 13:02:33 +0700

    create and edit event

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+import { RouterLink } from 'vue-router'
 
 interface Event {
   id: number
@@ -28,7 +29,9 @@ onMounted(() => {
     Home
 
     <div>Pendaftaran pondok pesantren Alberr</div>
-
+<div>
+      <RouterLink to="/event">Buat Event</RouterLink>
+    </div>
     <div>
       <ul>
         <li v-for="event in events" :key="event.id">
